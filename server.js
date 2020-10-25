04b1fcb87ba4cffb8cd9d3ea10035402b903c095
Truong Bao Lam <truongbaolam0209@gmail.com>
2020-10-25 14:57:32 +0700

    first commit

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+// const connectDB = require('./config/db');
+
+
+const app = express();
+
+
+app.get('/', (req, res) => { res.send('API running ...') });
+
+
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+    console.log(`Server started on port ${PORT}`);
+});
