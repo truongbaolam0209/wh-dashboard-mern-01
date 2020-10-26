086ece8d512dbac4c1d25b8d3e152bb44f0bce63
Truong Bao Lam <truongbaolam0209@gmail.com>
2020-10-26 23:41:06 +0700

    prepare heroku

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const connectDB = require('./config/db');
+const path = require('path');
+
+
 
 const app = express();
 
@@ -9,6 +12,20 @@ app.use(express.json({ extended: false }));
 
 
 app.use('/api/records', require('./routes/api/records'));
+
+
+
+
+// Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+    // Set static folder
+    app.use(express.static('client/build'));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+};
+
 
 
 
